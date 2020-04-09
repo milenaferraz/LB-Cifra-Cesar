7c2265986b2e0813451f9eda5f3be3d64a28d00f
Milena Ferraz <milenaferraz@gmail.com>
2020-04-09 08:12:44 -0300

    First Commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
-const cipher = {
-  // ...
-};
 
+const cipher = {
+
+  encode: (offset, string) => {
+      
+    if(offset == "" || offset <= 0 || string == "") {
+
+      throw TypeError("Preencha os campos corretamente!!");
+    }
+    
+    let result = "";
+    
+    for (var i  = 0; i  < string.length; i ++) {
+      let ascii = string.charCodeAt(i); 
+      let newAscii = (((ascii - 65) + offset) % 26) + 65;
+      let letterEncode = String.fromCharCode(newAscii);
+      result += letterEncode;
+    }
+    return result;
+   
+  },
+
+  decode: (offset, string) => {
+
+    if(offset == "" || offset <= 0 || string == "") {
+
+    throw TypeError("Preencha os campos corretamente!!");
+
+    }
+
+    let result = "";
+
+    for (var i  = 0; i  < string.length; i ++) {
+      let ascii = string.charCodeAt(i);
+      let newAscii = (((ascii + 65) - offset) % 26) + 65;
+      let letterDecode = String.fromCharCode(newAscii);
+      result += letterDecode;
+    }
+    return result;
+  }
+}
 export default cipher;
